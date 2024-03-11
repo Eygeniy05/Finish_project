31175e4c8bc046c7569e4e3beee8433c9f6ec743
Zhenya <prahlidera@mail.ru>
2024-03-11 22:42:15 +0300

    added helper functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string[] EnterArrayFromKeyboard() {
+    string [] array = new string [0];
+
+    Console.WriteLine("Введите элементы массива. Для выхода нажмите клавишу Q");
+
+    while(true) {
+        string str = Console.ReadLine();
+
+        if (str == "q") {
+            Console.WriteLine("[stopped]");
+            Console.WriteLine();
+            break;
+        }
+
+        Array.Resize(ref array, array.Length + 1);
+        array[array.Length - 1] = str;
+    }
+
+    return array;
+}
+
+static void PrintArray(string [] array) {
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write('"' + array[i] + '"');
+        if (i != array.Length - 1) {
+            Console.Write(", ");
+        }
+    }
+    
+    Console.Write("]");
+
+    Console.WriteLine();
+}
+
+static string[] FilterArrayByItemLength(string [] array, int limit) {
+    string [] result = new string [0];
+
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i].Length <= 3) {
+            Array.Resize(ref result, result.Length + 1);
+            result[result.Length - 1] = array[i];
+        }
+    }
+
+    return result;
+}
